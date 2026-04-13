ef6fd4fbd9dfb3a054652630060650aa43d11188
Javier Perez Arribillaga <22298889+dymlos@users.noreply.github.com>
2026-04-13 20:46:39 -0300

    Sprites de escapistas
    Se agregaron sprites de animales a los escapistas y se les sacó la mira que tenían

--- scenes/characters/escapist/escapist.gd
+++ scenes/characters/escapist/escapist.gd
@@ -259,6 +259,149 @@ func _find_rescue_target() -> Escapist:
 	return best
 
 
+func _get_animal_mark_alpha() -> float:
+	if not movement:
+		return 0.95
+	var speed := maxf(movement.velocity.length(), velocity.length())
+	if movement.is_dashing:
+		speed = movement.move_speed * 1.4
+	var move_ratio := clampf(speed / maxf(movement.move_speed, 1.0), 0.0, 1.0)
+	return lerpf(0.95, 0.48, move_ratio)
+
+
+func _make_animal_mark_color(base_color: Color, alpha_scale: float = 1.0) -> Color:
+	var alpha := _get_animal_mark_alpha() * alpha_scale
+	return Color(base_color, alpha)
+
+
+func _draw_animal_mark(base_color: Color) -> void:
+	var mark_color := _make_animal_mark_color(base_color)
+	match escapist_animal:
+		Enums.EscapistAnimal.RABBIT:
+			_draw_rabbit_mark(mark_color)
+		Enums.EscapistAnimal.RAT:
+			_draw_rat_mark(mark_color)
+		Enums.EscapistAnimal.SQUIRREL:
+			_draw_squirrel_mark(mark_color)
+		Enums.EscapistAnimal.FLY:
+			_draw_fly_mark(base_color)
+
+
+func _draw_rabbit_mark(mark_color: Color) -> void:
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(-5.8, -3.5),
+		Vector2(-10.8, -13.0),
+		Vector2(-6.0, -14.2),
+		Vector2(-2.0, -4.6),
+	]), mark_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(5.8, -3.5),
+		Vector2(10.8, -13.0),
+		Vector2(6.0, -14.2),
+		Vector2(2.0, -4.6),
+	]), mark_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(-8.0, -2.0),
+		Vector2(-4.0, -7.0),
+		Vector2(4.0, -7.0),
+		Vector2(8.0, -2.0),
+		Vector2(7.0, 5.5),
+		Vector2(2.0, 10.0),
+		Vector2(-2.0, 10.0),
+		Vector2(-7.0, 5.5),
+	]), mark_color)
+
+
+func _draw_filled_ellipse(center: Vector2, radii: Vector2, fill_color: Color, point_count: int = 18) -> void:
+	var points := PackedVector2Array()
+	for i in range(point_count):
+		var angle := TAU * float(i) / float(point_count)
+		points.append(center + Vector2(cos(angle) * radii.x, sin(angle) * radii.y))
+	draw_colored_polygon(points, fill_color)
+
+
+func _draw_rat_mark(mark_color: Color) -> void:
+	var eye_color := Color(0.01, 0.01, 0.01, mark_color.a)
+	var tail_color := Color(mark_color.r, mark_color.g, mark_color.b, mark_color.a * 0.55)
+	draw_polyline(PackedVector2Array([
+		Vector2(-6.2, 7.6),
+		Vector2(-12.8, 9.2),
+		Vector2(-15.0, 6.0),
+		Vector2(-9.4, 4.5),
+	]), tail_color, 2.4)
+	_draw_filled_ellipse(Vector2(-5.0, 2.4), Vector2(8.0, 8.8), mark_color)
+	_draw_filled_ellipse(Vector2(1.0, -3.8), Vector2(6.4, 4.8), mark_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(3.5, -7.4),
+		Vector2(13.8, -3.0),
+		Vector2(5.0, 1.4),
+	]), mark_color)
+	draw_circle(Vector2(-1.6, -7.6), 3.5, mark_color)
+	draw_circle(Vector2(2.4, -7.8), 3.2, mark_color)
+	draw_circle(Vector2(5.5, -4.8), 1.1, eye_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(2.0, 1.0),
+		Vector2(5.0, 1.6),
+		Vector2(3.5, 5.2),
+	]), mark_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(-8.0, 9.4),
+		Vector2(2.0, 9.0),
+		Vector2(-0.5, 11.0),
+	]), mark_color)
+
+
+func _draw_squirrel_mark(mark_color: Color) -> void:
+	var eye_color := Color(0.01, 0.01, 0.01, mark_color.a)
+	draw_arc(Vector2(-4.5, -2.0), 8.6, -PI * 0.18, PI * 1.6, 26, mark_color, 3.1)
+	draw_arc(Vector2(-5.0, -2.0), 4.4, PI * 0.82, PI * 1.95, 18, mark_color, 3.0)
+	draw_line(Vector2(-5.0, 2.0), Vector2(-5.0, 10.2), mark_color, 3.1)
+	_draw_filled_ellipse(Vector2(2.0, 5.0), Vector2(5.8, 6.0), mark_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(4.0, -4.0),
+		Vector2(8.0, -7.5),
+		Vector2(12.8, -3.0),
+		Vector2(9.5, 1.8),
+		Vector2(4.0, 1.0),
+	]), mark_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(5.6, -5.0),
+		Vector2(6.8, -10.0),
+		Vector2(9.0, -5.8),
+	]), mark_color)
+	draw_circle(Vector2(9.2, -2.5), 0.9, eye_color)
+	draw_polyline(PackedVector2Array([
+		Vector2(7.5, 1.2),
+		Vector2(10.8, 3.8),
+		Vector2(6.5, 4.8),
+	]), mark_color, 2.0)
+	draw_line(Vector2(3.0, 9.5), Vector2(9.0, 9.5), mark_color, 2.8)
+
+
+func _draw_fly_mark(base_color: Color) -> void:
+	var wing_color := _make_animal_mark_color(base_color, 0.62)
+	var body_color := _make_animal_mark_color(base_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(-1.5, -3.0),
+		Vector2(-12.5, -8.5),
+		Vector2(-14.0, 0.8),
+		Vector2(-5.0, 5.6),
+	]), wing_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(1.5, -3.0),
+		Vector2(12.5, -8.5),
+		Vector2(14.0, 0.8),
+		Vector2(5.0, 5.6),
+	]), wing_color)
+	draw_colored_polygon(PackedVector2Array([
+		Vector2(-2.8, -8.5),
+		Vector2(2.8, -8.5),
+		Vector2(4.0, 6.8),
+		Vector2(0.0, 11.0),
+		Vector2(-4.0, 6.8),
+	]), body_color)
+
+
 func _draw() -> void:
 	var animal_color := Enums.escapist_animal_color(escapist_animal)
 	var team_color := Enums.team_color(team)
@@ -273,14 +416,10 @@ func _draw() -> void:
 	if controls_inverted:
 		draw_color = draw_color.lerp(Color(1.0, 0.0, 1.0), 0.3)
 
-	# Circle with ring
-	draw_circle(Vector2.ZERO, Constants.CHARACTER_RADIUS - 2.0, draw_color)
-	draw_arc(Vector2.ZERO, Constants.CHARACTER_RADIUS + 2.0, 0, TAU, 16,
-		team_color, 2.0)
-
-	# Direction indicator
-	var tip := aim_direction * (Constants.CHARACTER_RADIUS + 8.0)
-	draw_line(Vector2.ZERO, tip, draw_color, 2.0)
+	# Animal mark with outer team ring
+	_draw_animal_mark(draw_color)
+	draw_arc(Vector2.ZERO, Constants.CHARACTER_RADIUS + 5.5, 0, TAU, 24,
+		Color(team_color, 0.55), 1.4)
 
 	# Label
 	var label := "P%d" % (player_index + 1)
@@ -309,8 +448,8 @@ func _draw() -> void:
 			_floating_text, HORIZONTAL_ALIGNMENT_LEFT, -1, text_size,
 			Color(_floating_text_color, text_alpha))
 
-	var ability_color := Color(0.2, 1.0, 0.4) if _ability_available else Color(0.35, 0.35, 0.35)
-	draw_arc(Vector2.ZERO, Constants.CHARACTER_RADIUS + 7.0, 0, TAU, 18, ability_color, 1.5)
+	var ability_color := Color(0.2, 1.0, 0.4, 0.45) if _ability_available else Color(0.45, 0.45, 0.45, 0.32)
+	draw_arc(Vector2.ZERO, Constants.CHARACTER_RADIUS + 8.5, 0, TAU, 24, ability_color, 1.2)
 	if _rabbit_charging:
 		var ratio := _rabbit_charge_time / Constants.RABBIT_LEAP_MAX_CHARGE
 		draw_arc(Vector2.ZERO, Constants.CHARACTER_RADIUS + 10.0,
